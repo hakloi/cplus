42dc3988cbb889525af990a3088c9029f74b6672
quir0r <murasakitt@gmail.com>
2024-09-15 23:50:21 +0300

    update

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main(){
+	// cout << sizeof(float) << endl;
+	// cout << numeric_limits<int>::max() << endl;
+	double a = 0.1 + 0.2;
+	cout << a << endl;
+}
