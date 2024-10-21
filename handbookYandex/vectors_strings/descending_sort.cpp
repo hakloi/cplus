d3194fdb34fbc0f5beeba80d30a07f4161bdcf46
quir0r <murasakitt@gmail.com>
2024-10-21 12:34:46 +0300

    update

---
+++ handbookYandex/vectors_strings/descending_sort.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    std::vector<std::string> lines;
+
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        lines.push_back(line);
+    }
+    // break ctrl + d or ctrl + z
+
+    std::sort(lines.rbegin(), lines.rend());
+
+    for (size_t i = 0; i != lines.size(); ++i) {
+        std::cout << lines[i] << "\n";
+    }
+}
