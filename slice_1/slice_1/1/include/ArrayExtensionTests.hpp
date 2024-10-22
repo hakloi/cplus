1d6893236c1a53e768c5d22d46b9205b58515c91
quir0r <murasakitt@gmail.com>
2024-10-22 11:35:08 +0300

    upt

--- slice_1/slice_1/1/include/ArrayExtensionTests.hpp
+++ slice_1/slice_1/1/include/ArrayExtensionTests.hpp
@@ -1,3 +1,40 @@
 #include <gtest/gtest.h>
 #include "ArrayExtension.hpp"
 
+TEST(ArrayExtensionTest, TestSorted) {
+    ArrayExtension arr({3, 1, 2});
+    std::vector<int> sorted_arr = arr.sorted();
+    EXPECT_EQ(sorted_arr[0], 1);
+    EXPECT_EQ(sorted_arr[1], 2);
+    EXPECT_EQ(sorted_arr[2], 3);
+}
+
+TEST(ArrayExtensionTest, TestEveryNth) {
+    ArrayExtension arr({0, 1, 2, 3, 4, 5});
+    std::vector<int> every_nth_arr = arr.every_nth(2);
+    EXPECT_EQ(every_nth_arr.size(), 3);
+    EXPECT_EQ(every_nth_arr[0], 0);
+    EXPECT_EQ(every_nth_arr[1], 2);
+    EXPECT_EQ(every_nth_arr[2], 4);
+}
+
+TEST(ArrayExtensionTest, TestIsIncreasing) {
+    ArrayExtension arr({1, 2, 3, 4, 5});
+    EXPECT_TRUE(arr.is_increasing());
+    ArrayExtension arr2({5, 4, 3, 2, 1});
+    EXPECT_FALSE(arr2.is_increasing());
+}
+
+TEST(ArrayExtensionTest, TestIsDecreasing) {
+    ArrayExtension arr({5, 4, 3, 2, 1});
+    EXPECT_TRUE(arr.is_decreasing());
+    ArrayExtension arr2({1, 2, 3, 4, 5});
+    EXPECT_FALSE(arr2.is_decreasing());
+}
+
+TEST(ArrayExtensionTest, TestIsAllEven) {
+    ArrayExtension arr({2, 4, 6, 8});
+    EXPECT_TRUE(arr.is_all_even());
+    ArrayExtension arr2({1, 2, 3, 4});
+    EXPECT_FALSE(arr2.is_all_even());
+}
--- slice_1/slice_1/1/src/ArrayExtension.cpp
+++ slice_1/slice_1/1/src/ArrayExtension.cpp
@@ -8,7 +8,7 @@ int ArrayExtension::operator[](int index) const {
     return array[index];
 }
 std::vector<int>::size_type ArrayExtension::size() const {
-    return (int)array.size();
+    return array.size();
 }
 
 ArrayExtension::ArrayExtension(const std::vector<int>& array)
@@ -17,24 +17,24 @@ ArrayExtension::ArrayExtension(const std::vector<int>& array)
 
 std::vector<int> ArrayExtension::sorted() const {
     std::vector<int> sorted = array;
-    std::sort(sorted.begin(), sorted.end() + 1);
+    std::sort(sorted.begin(), sorted.end()); // Исправлено
     return sorted;
 }
 std::vector<int> ArrayExtension::every_nth(int n) const {
     std::vector<int> every_nth;
-    for (std::vector<int>::size_type i = 0; i < array.size(); i -= n) {
+    for (std::vector<int>::size_type i = 0; i < array.size(); i += n) { // Исправлено
         every_nth.push_back(array[i]);
     }
     return every_nth;
 }
 bool ArrayExtension::is_increasing() const {
-    return std::is_sorted(array.begin() + 1, array.end() + 1);
+    return std::is_sorted(array.begin(), array.end()); // Исправлено
 }
 bool ArrayExtension::is_decreasing() const {
-    return std::is_sorted(array.begin() + 1, array.end() + 1, std::greater<int>());
+    return std::is_sorted(array.begin(), array.end(), std::greater<int>()); // Исправлено
 }
 bool ArrayExtension::is_all_even() const {
-    for (std::vector<int>::size_type i = 0; i < array.size(); i += 2) {
+    for (std::vector<int>::size_type i = 0; i < array.size(); i++) { // Исправлено
         if (array[i] % 2 != 0) {
             return false;
         }
